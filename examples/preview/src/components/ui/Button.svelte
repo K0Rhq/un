b73dcf7221aef46f4572f2ed4e956edcebbaf73a
dukc <dukkcc@proton.me>
2025-01-11 13:26:09 -0800

    Add cva button for testing inspector

---
+++ examples/preview/src/components/ui/Button.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type { HTMLButtonAttributes } from "svelte/elements";
+  import { cva, type VariantProps } from "class-variance-authority";
+
+  const button = cva("px-4 py-2 flex items-center justify-center gap-2", {
+    variants: {
+      variant: {
+        primary: "bg-green-800",
+        secondary: "bg-red-800",
+      },
+    },
+  });
+
+  interface $$Props extends HTMLButtonAttributes, VariantProps<typeof button> {}
+
+  /**
+   * For Svelte components, we recommend setting your defaultVariants within
+   * Svelte props (which are `undefined` by default)
+   */
+  export const { variant = "primary", ...rest } = $props();
+</script>
+
+<button {...rest} class={button({ variant })}>
+  {@render rest.children?.()}
+</button>

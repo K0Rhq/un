3f52a2f0eeb04a0b362228f0ab18f181e5a51a29
dukc <dukkcc@proton.me>
2025-02-05 20:17:17 -0800

    add readmes

--- examples/preview/src/stores/inspector.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from "nanostores";
-import type { PreviewSchema } from "@korhq/unpreview";
-import type { z } from "astro:content";
-
-type InferredPreviewSchema = z.infer<typeof PreviewSchema>;
-export const componentProps = atom<InferredPreviewSchema["props"]>(undefined);
---
+++ packages/preview/README.md
@@ -0,0 +1,5 @@
+# unPreview
+
+## v0.1.0-experimental
+
+Preview your components and UI docs via previews made in MDX, inside an Astro component playground that you own.

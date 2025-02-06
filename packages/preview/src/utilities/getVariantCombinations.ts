b03dde472645bade505629673cb97f0b45cc5040
dukc <dukkcc@proton.me>
2025-02-05 21:56:14 -0800

    fixes

--- packages/preview/src/utilities/getVariantCombinations.ts
+++ packages/preview/src/utilities/getVariantCombinations.ts
@@ -23,13 +23,16 @@ function generateVariantObjects<T>(
 
 /**
  * Generates all possible variant combinations from a variant configuration object
- * @param variantConfig An object where each key represents a variant property (e.g., 'size', 'color') and its value is an object containing an 'options' array of possible values
- } }
- * @returns An array of objects representing all possible combinations of variants
+ * @param variantConfig (preview.data.variants) An object where each key represents a variant property and its value is an object containing an 'options' array of possible values
+ * @returns An array of objects representing all possible combinations of   variants
  */
 export function generateVariants(
   variantConfig: Record<string, { options: unknown[] }>,
 ) {
+  if (!variantConfig) {
+    return [];
+  }
+
   const variantKeys = Object.keys(variantConfig);
   const optionsArrays = variantKeys.map(
     (key) => variantConfig[key]?.options ?? [],
